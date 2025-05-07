b18c1e57d6e234ec38fcc44e82d7e6e63a9a4518
Bruno Oliveira <brunooliver2015@outlook.comm>
2025-05-07 15:26:14 -0300

    hero atualizado

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../index.css";
 import { FaInstagram, FaFacebook } from "react-icons/fa";
 
-
 const Contato = () => {
   return (
     <section id="contato" className="contato-container">
@@ -10,27 +9,31 @@ const Contato = () => {
 
       <div className="contato-info">
         <p>
-          📍 <strong>Endereço:</strong> Av. Florestan Fernandes, 812 - Jardim Pte. Alta I, Guarulhos - SP
+          📍 <strong>Endereço:</strong> Av. Florestan Fernandes, 812 - Jardim
+          Pte. Alta I, Guarulhos - SP
         </p>
         <p>
           📞 <strong>Telefone / WhatsApp:</strong>{" "}
-          <a href="https://wa.me/5511999999999" target="_blank" rel="noreferrer">
+          <a
+            href="https://wa.me/5511976203951"
+            target="_blank"
+            rel="noreferrer"
+          >
             (11) 97620-3951
           </a>
         </p>
       </div>
 
       <div className="mapa">
-      <iframe
-  title="Localização da Pizzaria"
-  src="https://www.google.com/maps/embed?pb=SEU_CODIGO_COMPLETO"
-  width="100%"
-  height="300"
-  style={{ border: 0 }}
-  allowFullScreen
-  loading="lazy"
-></iframe>
-
+        <iframe
+          title="Localização da Pizzaria"
+          src="https://www.google.com/maps/place/Av.+Florestan+Fernandes,+812+-+Jardim+Pte.+Alta+I,+Guarulhos+-+SP/@-23.4093312,-46.4183827,1006m/data=!3m2!1e3!4b1!4m5!3m4!1s0x94ce89bc0d1a3b9d:0x9c95f9d9cffe5c20!8m2!3d-23.4093312!4d-46.4158078?entry=ttu&g_ep=EgoyMDI1MDUwNS4wIKXMDSoASAFQAw%3D%3D"
+          width="100%"
+          height="300"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+        ></iframe>
       </div>
 
       <div className="horario-redes">
@@ -40,15 +43,15 @@ const Contato = () => {
         </div>
 
         <div className="redes-sociais">
-  <h3>📱 Siga a gente</h3>
-  <a
-    href="https://www.instagram.com/pizzariadonatello_ofc/"
-    target="_blank"
-    rel="noreferrer"
-  >
-    <FaInstagram style={{ marginRight: "8px" }} />
-    Instagram
-  </a>
+          <h3>📱 Siga a gente</h3>
+          <a
+            href="https://www.instagram.com/pizzariadonatello_ofc/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaInstagram style={{ marginRight: "8px" }} />
+            Instagram
+          </a>
         </div>
       </div>
     </section>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,15 @@ function Hero() {
     <section className="hero">
       <div className="hero-content">
         <h1>As melhores pizzas da cidade 🍕</h1>
-        <p>Feitas com ingredientes selecionados e aquele toque especial do DINA!</p>
-        <a href="#cardapio" className="btn">Ver Cardápio</a>
+        <p>
+          Feitas com ingredientes selecionados e aquele toque especial do
+          Donatello!
+        </p>
+        <a href="#cardapio" className="btn">
+          Ver Cardápio
+        </a>
       </div>
     </section>
   );
 }
 export default Hero;
-
-  
