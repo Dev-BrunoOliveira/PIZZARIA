1086ff92315680560bd2d595dc791d518b8a1068
Bruno Oliveira <brunooliver2015@outlook.comm>
2025-05-09 23:59:40 -0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,67 @@ import Hero from "./components/Hero";
 import Cardapio from "./components/Cardapio";
 import Contato from "./components/Contato";
 
-
 function App() {
+  const pizzas = [
+    {
+      nome: "Margherita",
+      descricao: "Molho de tomate, mussarela, manjericão fresco",
+      preco: "R$ 39,90",
+      img: "/img/Margherita.jpg",
+    },
+    {
+      nome: "Calabresa",
+      descricao: "Calabresa artesanal, cebola, orégano",
+      preco: "R$ 42,00",
+      img: "/img/Calabresa.jpg",
+    },
+    {
+      nome: "Portuguesa",
+      descricao: "Presunto de Parma, ervilha e um ovo estalado por cima",
+      preco: "R$ 42,00",
+      img: "/img/Portuguesa.jpg",
+    },
+    {
+      nome: "Baiana",
+      descricao:
+        "Linguiça calabresa, cebola, tomate, pimentão assado, e pimenta calabresa",
+      preco: "R$ 47,00",
+      img: "/img/Baiana.jpg",
+    },
+    {
+      nome: "Mussarela Clássica",
+      descricao: "Mussarela, orégano, azeitonas",
+      preco: "R$ 47,00",
+      img: "/img/Mussarela.jpg",
+    },
+    {
+      nome: "Frango com Catupiry",
+      descricao: "Frango, catupiry, orégano e azeitonas",
+      preco: "R$ 47,00",
+      img: "/img/FrangoCatupiry.png",
+    },
+    {
+      nome: "Pepperoni",
+      descricao: "Pepperoni, molho e muito queijo",
+      preco: "R$ 47,00",
+      img: "/img/Pepperoni.jpg",
+    },
+    {
+      nome: "Lombo",
+      descricao: "Lombo, queijo e molho de tomate",
+      preco: "R$ 47,00",
+      img: "/img/Lombo.jpg",
+    },
+  ];
+
   return (
     <>
       <Navbar />
       <Hero />
-      <Cardapio />
+      <Cardapio pizza={pizzas} />
       <Contato />
-  
     </>
   );
 }
 
 export default App;
-
--- src/components/Cardapio.jsx
+++ src/components/Cardapio.jsx
@@ -1,80 +1,17 @@
-function Cardapio() {
+
+function Cardapio({ pizza }) {
   return (
     <section id="cardapio" className="sessao">
       <h2>Cardápio</h2>
       <div className="pizzas">
-        <div className="pizza">
-          <img
-            src="/img/Margherita.jpg" alt="imagem-ilustrativa"
-            className="img"
-          />
-          <h3>Margherita</h3>
-          <p>Molho de tomate, mussarela, manjericão fresco</p>
-          <span>R$ 39,90</span>
-        </div>
-        <div className="pizza">
-          <img
-            src="/img/Calabresa.jpg" alt="imagem-ilustrativa"
-            className="img"
-          />
-          <h3>Calabresa</h3>
-          <p>Calabresa artesanal, cebola, orégano</p>
-          <span>R$ 42,00</span>
-        </div>
-        <div className="pizza">
-          <img
-            src="/img/Portuguesa.jpg" alt="imagem-ilustrativa"
-            className="img"
-          />
-          <h3>Portuguesa</h3>
-          <p>Presunto de Parma, ervilha e um ovo estalado por cima</p>
-          <span>R$ 42,00</span>
-        </div>
-        <div className="pizza">
-          <img
-            src="/img/Baiana.jpg" alt="imagem-ilustrativa"
-            className="img"
-          />
-          <h3>Baiana</h3>
-          <p>Linguiça calabresa, cebola, tomate, pimentão assado, e pimenta calabresa</p>
-          <span>R$ 47,00</span>
-        </div>
-        <div className="pizza">
-          <img
-            src="/img/Mussarela.jpg" alt="imagem-ilustrativa"
-            className="img"
-          />
-          <h3>Mussarela Clássica</h3>
-          <p>Mussarela, orégano, azeitonas</p>
-          <span>R$ 47,00</span>
-        </div>
-        <div className="pizza">
-          <img
-            src="/img/FrangoCatupiry.png" alt="imagem-ilustrativa"
-            className="img"
-          />
-          <h3>Frango com Catupiry</h3>
-          <p>Frango, catupiry, orégano e azeitonas</p>
-          <span>R$ 47,00</span>
-        </div>
-        <div className="pizza">
-          <img
-            src="/img/Pepperoni.jpg" alt="imagem-ilustrativa"
-            className="img"
-          />
-          <h3>Pepperoni</h3>
-          <p>Pepperoni,molho e Muito queijo</p>
-          <span>R$ 47,00</span>
-        </div>
-        <div className="pizza">
-          <img
-            src="/img/Lombo.jpg" alt="imagem-ilustrativa"
-            className="img"
-          />
-          <h3>Lombo</h3>
-          <p>Lombo, queijo e molhode tomate</p>
-          <span>R$ 47,00</span>
-        </div>
+        {pizza.map((item, index) => (
+          <div key={index} className="pizza">
+            <img src={item.img} alt="imagem-ilustrativa" className="img" />
+            <h3>{item.nome}</h3>
+            <p>{item.descricao}</p>
+            <span>{item.preco}</span>
+          </div>
+        ))}
       </div>
     </section>
   );
